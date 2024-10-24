4309b2dc49f5df7ce59ecf0a057884190382eadc
Michael Kavulich <kavulich@ucar.edu>
2024-10-24 13:08:58 -0600

    More improvements to plotting script
    * Add a sanity check to load_wflow_modules.sh for when platform is not specified
    
    * Add a defaults file, fix bug in filename directory check, add some
    documentation.
    
    * - Add gitignore for image files
     - Add option periodic_bdy to plot all boundary points; this is false by
    default because it only applies to periodic (global) domains and it
    substantially slows down the plotting routine.
    
    * Rename default config to config_plot.yaml
    
    * Fix capability of plotting multiple levels (not just "all")
    
    * Add note about uxarray load time

--- plot_mpas_netcdf.py
+++ plot_mpas_netcdf.py
@@ -10,6 +10,7 @@ import os
 import sys
 import time
 
+print("Importing uxarray; this may take a while...")
 import uxarray as ux
 import matplotlib.pyplot as plt
 
@@ -76,36 +77,44 @@ def plotit(config_d: dict,uxds: ux.UxDataset,filepath: str) -> None:
                 logging.info(f"Variable {var} not face-centered, skipping")
                 continue
             logging.info(f"Plotting variable {var}")
-            sliced=uxds[var]
-            if "Time" in sliced.dims:
+            field=uxds[var]
+            # If multiple timesteps in a file, only plot the first for now
+            if "Time" in field.dims:
                 logging.info("Plotting first time step")
-                sliced=sliced.isel(Time=0)
-            lev=0
-            if "nVertLevels" in sliced.dims:
+                field=field.isel(Time=0)
+            # Parse multiple levels for 3d fields
+            # "sliced" is a dictionary of 2d slices of data we will plot. We use a dictionary
+            # instead of a list because the levels may not necessarily be contiguous or monotonic
+            sliced = {}
+            if "nVertLevels" in field.dims:
                 if config_d["data"]["lev"]:
-                    lev=config_d["data"]["lev"][0]
-                logging.info(f'Plotting vertical level {lev}')
-                sliced=sliced.isel(nVertLevels=lev)
+                    levs=config_d["data"]["lev"]
+                logging.info(f'Plotting vertical level(s) {levs}')
+                for lev in levs:
+                    sliced[lev]=field.isel(nVertLevels=lev)
+            else:
+                levs = [0]
+                sliced[0]=field
 
+            for lev in levs:
+                logging.debug(f"For level {lev}, data slice to plot:\n{sliced[lev]}")
+                if config_d["plot"]["periodic_bdy"]:
+                    pc=sliced[lev].to_polycollection(periodic_elements='split')
+                else:
+                    pc=sliced[lev].to_polycollection()
 
-#            logging.info(f"Timer 1 {time.time()-start}")
-            logging.debug(sliced)
-#            logging.info(f"Timer 2 {time.time()-start}")
-            pc=sliced.to_polycollection()
-#            logging.info(f"Timer 3 {time.time()-start}")
+                pc.set_antialiased(False)
 
-            pc.set_antialiased(False)
-
-            pc.set_cmap(config_d["plot"]["colormap"])
+                pc.set_cmap(config_d["plot"]["colormap"])
 
 #            logging.info(f"Timer 4 {time.time()-start}")
-            fig, ax = plt.subplots(1, 1, figsize=(config_d["plot"]["figwidth"], config_d["plot"]["figheight"]),
+                fig, ax = plt.subplots(1, 1, figsize=(config_d["plot"]["figwidth"], config_d["plot"]["figheight"]),
                                    dpi=config_d["plot"]["dpi"], constrained_layout=True)
 #            logging.info(f"Timer 5 {time.time()-start}")
 
 
-            ax.set_xlim((config_d["plot"]["lonrange"][0],config_d["plot"]["lonrange"][1]))
-            ax.set_ylim((config_d["plot"]["latrange"][0],config_d["plot"]["latrange"][1]))
+                ax.set_xlim((config_d["plot"]["lonrange"][0],config_d["plot"]["lonrange"][1]))
+                ax.set_ylim((config_d["plot"]["latrange"][0],config_d["plot"]["latrange"][1]))
 
             # add geographic features
         #    ax.add_feature(cfeature.COASTLINE)
@@ -113,34 +122,36 @@ def plotit(config_d: dict,uxds: ux.UxDataset,filepath: str) -> None:
 
             # Create a dict of substitutable patterns to make string substitutions easier
             # using the python string builtin method format_map()
-            patterns = {
-                "var": var,
-                "lev": lev,
-                "units": uxds[var].attrs["units"],
-                "varln": uxds[var].attrs["long_name"],
-                "filename": filename,
-                "fnme": fnme,
-                "date": uxds[var].coords['Time'].dt.strftime('%Y-%m-%d').values[0],
-                "time": uxds[var].coords['Time'].dt.strftime('%H:%M:%S').values[0]
-            }
-
-            coll = ax.add_collection(pc)
-
-            plottitle=config_d["plot"]["title"].format_map(patterns)
-            plt.title(plottitle, wrap=True)
-
-            # Handle colorbar
-            if config_d["plot"].get("colorbar"):
-                cb = config_d["plot"]["colorbar"]
-                cbar = plt.colorbar(coll,ax=ax,orientation=cb["orientation"])
-                if cb.get("label"):
-                    cbar.set_label(cb["label"].format_map(patterns))
-
-            outfile=config_d["plot"]["filename"].format_map(patterns)
-            # Make sure any subdirectories exist before we try to write the file
-            os.makedirs(os.path.dirname(outfile),exist_ok=True)
-            plt.savefig(outfile)
-            plt.close()
+                patterns = {
+                    "var": var,
+                    "lev": lev,
+                    "units": uxds[var].attrs["units"],
+                    "varln": uxds[var].attrs["long_name"],
+                    "filename": filename,
+                    "fnme": fnme,
+                    "date": uxds[var].coords['Time'].dt.strftime('%Y-%m-%d').values[0],
+                    "time": uxds[var].coords['Time'].dt.strftime('%H:%M:%S').values[0]
+                }
+    
+                coll = ax.add_collection(pc)
+    
+                plottitle=config_d["plot"]["title"].format_map(patterns)
+                plt.title(plottitle, wrap=True)
+    
+                # Handle colorbar
+                if config_d["plot"].get("colorbar"):
+                    cb = config_d["plot"]["colorbar"]
+                    cbar = plt.colorbar(coll,ax=ax,orientation=cb["orientation"])
+                    if cb.get("label"):
+                        cbar.set_label(cb["label"].format_map(patterns))
+    
+                outfile=config_d["plot"]["filename"].format_map(patterns)
+                # Make sure any subdirectories exist before we try to write the file
+                logging.debug(f"Saving plot {outfile}")
+                if os.path.dirname(outfile):
+                    os.makedirs(os.path.dirname(outfile),exist_ok=True)
+                plt.savefig(outfile)
+                plt.close()
 
 
     else:
@@ -183,25 +194,45 @@ def setup_logging(logfile: str = "log.generate_FV3LAM_wflow", debug: bool = Fals
     return logger
 
 
-def setup_config(config: str) -> dict:
+def setup_config(config: str, default: str="default_options.yaml") -> dict:
     """
     Function for reading in dictionary of configuration settings, and performing basic checks
     on those settings
+
+    Args:
+        config  (str) : The full path of the user config file
+        default (str) : The full path of the default config file
+        debug   (bool): Enable extra output for debugging
+    Returns:
+        dict: A dictionary of the configuration settings after applying defaults and user settings,
+              as well as some basic consistency checks
     """
-    logging.debug(f"Reading options file {config}")
+    logging.debug(f"Reading defaults file {default}")
     try:
-        config_d = uwconfig.get_yaml_config(config=config)
+        expt_config = uwconfig.get_yaml_config(config=default)
     except Exception as e:
         logging.critical(e)
         logging.critical(f"Error reading {config}, check above error trace for details")
         sys.exit(1)
-    if not config_d["data"].get("lev"):
+    logging.debug(f"Reading options file {config}")
+    try:
+        user_config = uwconfig.get_yaml_config(config=config)
+    except Exception as e:
+        logging.critical(e)
+        logging.critical(f"Error reading {config}, check above error trace for details")
+        sys.exit(1)
+
+    # Update the dict read from defaults file with the dict read from user config file
+    expt_config.update_values(user_config)
+
+    # Perform consistency checks
+    if not expt_config["data"].get("lev"):
         logging.debug("Level not specified in config, will use level 0 if multiple found")
-        config_d["data"]["lev"]=0
+        expt_config["data"]["lev"]=0
 
     logging.debug("Expanding references to other variables and Jinja templates")
-    config_d.dereference()
-    return config_d
+    expt_config.dereference()
+    return expt_config
 
 if __name__ == "__main__":
 
@@ -210,7 +241,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Script for plotting MPAS input and/or output in native NetCDF format"
     )
-    parser.add_argument('-c', '--config', type=str, default='plot_options.yaml',
+    parser.add_argument('-c', '--config', type=str, default='config_plot.yaml',
                         help='File used to specify plotting options')
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Script will be run in debug mode with more verbose output')
@@ -224,24 +255,24 @@ if __name__ == "__main__":
 
 
     # Load settings from config file
-    confg_d=setup_config(args.config)
+    expt_config=setup_config(args.config)
 
-    if os.path.isfile(confg_d["data"]["filename"]):
-        files = [confg_d["data"]["filename"]]
-    elif glob.glob(confg_d["data"]["filename"]):
-        files = sorted(glob.glob(confg_d["data"]["filename"]))
-    elif isinstance(confg_d["data"]["filename"], list):
-        files = confg_d["data"]["filename"]
+    if os.path.isfile(expt_config["data"]["filename"]):
+        files = [expt_config["data"]["filename"]]
+    elif glob.glob(expt_config["data"]["filename"]):
+        files = sorted(glob.glob(expt_config["data"]["filename"]))
+    elif isinstance(expt_config["data"]["filename"], list):
+        files = expt_config["data"]["filename"]
     else:
-        raise FileNotFoundError(f"Invalid filename(s) specified:\n{config_d['data']['filename']}")
+        raise FileNotFoundError(f"Invalid filename(s) specified:\n{expt_config['data']['filename']}")
 
-    if not confg_d["data"].get("gridfile"):
-        confg_d["data"]["gridfile"]=""
+    if not expt_config["data"].get("gridfile"):
+        expt_config["data"]["gridfile"]=""
 
     for f in files:
         # Open specified file and load dataset
-        dataset=load_dataset(f,confg_d["data"]["gridfile"])
+        dataset=load_dataset(f,expt_config["data"]["gridfile"])
 
 
         # Make the plots!
-        plotit(confg_d,dataset,f)
+        plotit(expt_config,dataset,f)
